0b26d4852cbc7561a536d80fde86583802a21b4c
Nagireddy Poojitha <112623812+Nagireddypoojitha1725@users.noreply.github.com>
2023-04-29 10:52:13 +0530

    Add files via upload

---
+++ bar chart.R
@@ -0,0 +1,5 @@
+x <-c(12,23,14)
+png(file = "barchart.png")
+barplot(x)
+dev.off()
+
---
+++ boxplot.R
@@ -0,0 +1,6 @@
+png(file = "boxplot.png")
+
+boxplot(mpg ~ cyl, data = mtcars, xlab = "Number of Cylinders",
+        ylab = "Miles Per Gallon", main = "Mileage Data")
+dev.off()
+
